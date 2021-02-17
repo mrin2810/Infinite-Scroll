22e83b6b095029efae7ce5abb8ea7b649f049be0
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2021-02-18 01:00:53 +0530

    added api request and fetched photos 10

---
+++ script.js
@@ -0,0 +1,53 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+// Unsplash API
+const count = 10;
+const apiKey = 'bYeFGilIRcmmReWJoN-eTEvWL3RBljcwLKwMWPeXFmc';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Helper function
+function setAttributes(element, attributes){
+    for (const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// display photos
+function displayPhotos() {
+    // Run function for each object
+    photosArray.forEach((photo) => {
+        // Create <a> elem to link to unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html, 
+            target: '_blank'
+        });
+        // Create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular, 
+            alt: photo.alt_description, 
+            title: photo.alt_description
+        });
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+// Get Photos
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+
+    }
+} 
+
+
+// on load
+getPhotos();
