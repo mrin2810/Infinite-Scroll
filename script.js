2a90b28e6cd4f26b074b692d670eeff1454f24cf
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2021-02-18 01:14:31 +0530

    infinite scroll functionality

--- script.js
+++ script.js
@@ -49,5 +49,13 @@ async function getPhotos() {
 } 
 
 
+// Scroll even
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+        getPhotos();
+        console.log('load more')
+    }
+})
+
 // on load
 getPhotos();
